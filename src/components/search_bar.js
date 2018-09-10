69d32479965ffef8724fe673a53afd7d19c82c1e
dkang417 <dkang417@gmail.com>
2018-09-10 10:49:44 -0400

    update searchbar component with render method

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <input />;
-};
+class SearchBar extends Component {
+    // need to render and return jsx
+
+    constructor(props) {
+        super(props);
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value })} />
+                {/* Value of the input: {this.state.term} */}
+            </div>
+        );
+    }
+
+
+}
 
 export default SearchBar;
 
