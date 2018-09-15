7f0712521b55b456b63da0c814610e26a4b8c8cb
dkang417 <dkang417@gmail.com>
2018-09-14 21:42:28 -0400

    added video details and list features with styling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBkQGAt3Lyb_WGpNbz0byzxFMqF9pr_F6k';
 
 // create a new component. this component should produce
@@ -13,10 +13,16 @@ const API_KEY = 'AIzaSyBkQGAt3Lyb_WGpNbz0byzxFMqF9pr_F6k';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
         
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-            this.setState({ videos: videos });
+        YTSearch({ key: API_KEY, term: 'new york giants' }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
             // es6 this.setState({videos}); if key value is same
         });
     }
@@ -24,7 +30,10 @@ class App extends Component {
         return(
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />     
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }    
+                    videos={this.state.videos} />     
             </div>
         );
     }
